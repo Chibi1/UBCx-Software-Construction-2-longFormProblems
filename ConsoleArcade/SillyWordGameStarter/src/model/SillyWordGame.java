47da83b8c749fa4486d30586d8bbd64f11f34c84
Chibi1 <chibueze.e.njoku@gmail.com>
2019-06-04 15:12:13 +1000

    implemented disegn pattern for phrase class

--- ConsoleArcade/SillyWordGameStarter/src/model/SillyWordGame.java
+++ ConsoleArcade/SillyWordGameStarter/src/model/SillyWordGame.java
@@ -1,20 +1,14 @@
 package model;
 
+import java.util.Iterator;
 import java.util.List;
 
-public class SillyWordGame {
+public class SillyWordGame implements Iterable<Phrase> {
 
     private List<Phrase> phrases;
-    //TODO: remove these fields
-    private int currentPhraseIndex;
-    private int numWordsNeeded;
 
     public SillyWordGame(List<Phrase> phrases) {
         this.phrases = phrases;
-        for(Phrase p : phrases) {
-            if (p.needsWord())
-                numWordsNeeded++;
-        }
     }
 
     //EFFECTS: returns all phrases in this game
@@ -22,24 +16,45 @@ public class SillyWordGame {
         return phrases;
     }
 
-    //MODIFIES: this
-    //EFFECTS: returns the next phrase in this game that needs a word
-    //TODO: remove this method
-    public Phrase getNextPhraseNeedingWord() {
-        for (int i = currentPhraseIndex; i < phrases.size(); i++){
-            if (!phrases.get(currentPhraseIndex).needsWord()){
-                currentPhraseIndex++;
-            } else {
-                numWordsNeeded--;
-                return phrases.get(currentPhraseIndex++);
-            }
-        }
-        throw new IllegalStateException();
+    @Override
+    public Iterator<Phrase> iterator() {
+        return new PhrasesNeedingWordsIterator();
     }
 
-    //EFFECTS: returns true if more words are needed
-    //TODO: remove this method
-    public boolean needMoreWords() {
-        return numWordsNeeded > 0;
+
+    private class PhrasesNeedingWordsIterator implements Iterator<Phrase> {
+        private int numWordsNeeded;
+        Iterator phraseIterator;
+
+        private PhrasesNeedingWordsIterator() {
+            phraseIterator = phrases.iterator();
+            calculateNumWordsNeeded();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (numWordsNeeded > 0);
+        }
+
+        @Override
+        public Phrase next() {
+            Phrase phrase = (Phrase) phraseIterator.next();
+            while (!phrase.needsWord()) {
+                phrase = (Phrase) phraseIterator.next();
+            }
+            numWordsNeeded--;
+            return phrase;
+        }
+
+        private void calculateNumWordsNeeded() {
+            Iterator<Phrase> iterator = phrases.iterator();
+            while (iterator.hasNext()) {
+                if(iterator.next().needsWord()) {
+                    numWordsNeeded++;
+                }
+            }
+        }
     }
+
+
 }
